1b3721b91fabf6bc4308af326b2f400165073a20
marcosumali <marcosumali.github.io>
2019-12-05 18:56:05 +0700

    revising security by adding env for API key. redeploy. revising readme file to proper documentation.

--- client/src/store/user/user.actions.js
+++ client/src/store/user/user.actions.js
@@ -1,6 +1,9 @@
 // PURPOSE: Actions documented in this store section is specialised for User State Management purposes
 import axios from 'axios';
 
+// API Key is stored on env file to be secured
+const GIPHY_API_KEY = process.env.REACT_APP_APIKEY
+
 // To handle store changes in form input during searching images
 export const handleChangesSearch = (e) => {
   return (dispatch) => {
@@ -30,7 +33,7 @@ export const getGiphyImages = (e, searchInput) => {
 
     dispatch(setGiphyImagesLoadingStatus(true))
     
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=JPj9Ot5N6th4djaq4QL33fcdglVU5a37&limit=8`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=${GIPHY_API_KEY}&limit=8`)
       .then(response => {
         // console.log(response)
         dispatch(setGiphyImagesLoadingStatus(false))
@@ -107,7 +110,7 @@ export const getGiphyImageFavourites = (imageIds) => {
   return (dispatch) => {
 
     let GIPHY_Ids = imageIds.join(',')
-    axios.get(`http://api.giphy.com/v1/gifs?api_key=JPj9Ot5N6th4djaq4QL33fcdglVU5a37&ids=${GIPHY_Ids}`)
+    axios.get(`https://api.giphy.com/v1/gifs?api_key=${GIPHY_API_KEY}&ids=${GIPHY_Ids}`)
       .then(response => {
         dispatch(setImagesFavLoadingStatus(false))
         dispatch(setImagesFav(response.data.data))
@@ -140,7 +143,7 @@ export const fetchMoreGiphyImage = (e, searchInput, images) => {
     e.preventDefault()
     dispatch(setFetchMoreImagesLoadingStatus(true))
 
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=JPj9Ot5N6th4djaq4QL33fcdglVU5a37&limit=8&offset=${images.length}`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=${GIPHY_API_KEY}&limit=8&offset=${images.length}`)
       .then(response => {
         // console.log(response)
         let newImages = [
